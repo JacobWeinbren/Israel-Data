4e15f3ce386799114094966f6cc226ce39eefed4
Jacob Weinbren <jacobweinbren@gmail.com>
2021-05-30 19:01:12 +0100

    Established Bounds

--- geocode.py
+++ geocode.py
@@ -1,16 +1,21 @@
-import googlemaps
-
 #Israel
 #34.2654333839, 29.5013261988, 35.8363969256, 33.2774264593
 
 #Palestine
 #31.2201289  32.5521479  34.0689732  35.5739235
 
-import key
+from key import gmaps
 
 def geocode_process(search):
-    result = gmaps.geocode(search, components={"country": ['IL', 'PS']}, bounds={"southwest": (29.003327, 32.922908),"northeast": (34.293521, 37.130672)}, region:"il")
+    result = gmaps.geocode(
+        search, 
+        components={"country": ['IL', 'PS']}, 
+        bounds={"southwest": (29.159416, 33.947754),"northeast": (33.705577, 36.365314)}, 
+        region="il"
+    )
     if len(result):
         return result[0]['geometry']['location']
     else:
         return None
+
+print(geocode_process('רבי צדוק,12 ‭'))
